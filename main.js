ccf247d431b4a9c737d4e5f70b58e4657c2e0903
Doloortiz <ortizdolo06@gmail.com>
2024-09-03 18:44:12 -0300

    cambie 1er card query

---
+++ main.js
@@ -0,0 +1,5 @@
+const h5 = document.querySelector("h5");
+const text = document.querySelector(".card-text");
+
+h5.innerText = "Bodas";
+text.innerText = "La torta que necesitas para tu casamiento.";
