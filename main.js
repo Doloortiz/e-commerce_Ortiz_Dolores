53876fb7e585574b97a7aa1716cf08a0b3aa37ad
Doloortiz <ortizdolo06@gmail.com>
2024-09-17 16:51:25 -0300

    sprint1

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-const h5 = document.querySelector("h5");
-const text = document.querySelector(".card-text");
+const h1 = document.querySelector("h1");
+h1.innerText = "Productos";
 
-h5.innerText = "Bodas";
-text.innerText = "La torta que necesitas para tu casamiento.";
+const array = [];
+for (let i = 1 ; i < 10 ; i++) {
+
+    const cards =
+        <div class="card" style="width: 18rem;">
+            <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top" alt="..."/>
+            <div class="card-body">
+                <h5 class="card-title">Card ${i}</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    
+    array.push(cards);
+} 
+document.querySelector("section").innerHTML = array.join('');
+
+// Selecciona la section y reemplaza su contenido con las tarjetas que se almacenaron en el Array.
