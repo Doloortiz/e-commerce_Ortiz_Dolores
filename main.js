17f50a1ad645e84ba8742292a55e05bbd0abad60
Doloortiz <ortizdolo06@gmail.com>
2024-10-01 18:24:23 -0300

    clase 10

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+function sprint1 () { 
 const h1 = document.querySelector("h1");
 h1.innerText = "Productos";
 
@@ -10,12 +11,14 @@ for (let i = 1 ; i < 10 ; i++) {
             <div class="card-body">
                 <h5 class="card-title">Card ${i}</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="#" class="btn btn-primary">Ver mas</a>
             </div>
         </div>
     
     array.push(cards);
 } 
 document.querySelector("section").innerHTML = array.join('');
+}
+sprint1 ()
 
 // Selecciona la section y reemplaza su contenido con las tarjetas que se almacenaron en el Array.
